f66a642358c62a70488567fa77b5bd27e5c4bbff
Muhammad Inam <nudmaufon@gmail.com>
2024-10-01 00:33:18 +0500

    functions and practice questions

--- Project2/functions.cpp
+++ Project2/functions.cpp
@@ -42,28 +42,83 @@ using namespace std;
 
 
 // Bionomail confficient 
-int factorialNumber(int num, int rc) {
-	int facN = 1;
-	int facR = 1;
-	int facNRCS = 1;
-	int initialResult = 0;
-	for (int i = 1; i <= num; i++) {
-		facN *= i;
-	}	
+//int factorialNumber(int num, int rc) {
+//	int facN = 1;
+//	int facR = 1;
+//	int facNRCS = 1;
+//	int initialResult = 0;
+//	for (int i = 1; i <= num; i++) {
+//		facN *= i;
+//	}	
+//
+//	for (int i = 1; i <= rc; i++) {
+//		facR *= i;
+//	}
+//	
+//	int facNRC = num - rc;
+//	for (int i = 1; i <= facNRC; i++) {
+//		facNRCS *= i;
+//	}
+//
+//	initialResult = facR * facNRCS;
+//	int result = facN / initialResult;
+//	return result;
+//
+//
+//}
 
-	for (int i = 1; i <= rc; i++) {
-		facR *= i;
-	}
-	
-	int facNRC = num - rc;
-	for (int i = 1; i <= facNRC; i++) {
-		facNRCS *= i;
-	}
+// check if a number is Prime or Not;
+int Prime(int num) {
+    // Handle edge cases
+    if (num <= 1) {
+        cout << num << " is not a prime number (0 and 1 are not prime)." << endl;
+        return 0;
+    }
 
-	initialResult = facR * facNRCS;
-	int result = facN / initialResult;
-	return result;
+    if (num == 2 || num == 3) {
+        cout << num << " is a prime number." << endl;
+        return 1;  // Return 1 to indicate it's prime
+    }
 
+    // Check for divisibility starting from 2
+    for (int i = 2; i * i <= num; i++) {
+        if (num % i == 0) {
+            cout << num << " is not a prime number." << endl;
+            return 0;  // Return 0 to indicate it's not prime
+        }
+    }
+
+    // If no divisors found, it's prime
+    cout << num << " is a prime number." << endl;
+    return 1;
+}
+
+// Question print all Prime numbers from 2 to N
+
+bool isPrime(int num) {
+    if (num <= 1) {
+        return false; // 0 and 1 is not Prime;
+    }
+    for (int i = 2; i * i <= num; i++) {
+        if (num % i == 0) {
+            return false; // not Prime number;
+        }
+
+
+    }
+    return true; // prime number
+}
+
+// Question find the fibonacci number;
+void printFibonacci(int N) {
+    int a = 0, b = 1, NextTerm;
+    for (int i = 4; i <= N; i++) {
+        NextTerm = a + b;
+        cout << NextTerm << " " << endl;
+        a = b;
+        b = NextTerm;
+    }
+    cout << endl;
 
 }
 
@@ -74,6 +129,22 @@ int main() {
 	//cout << sumNumber(5) << endl;
 	//cout << factorialNumber(3) << endl;
 	//cout << sumDigits(2356) << endl;
-	cout << factorialNumber(8, 2) << endl;
-	return 0;
-}
+	//cout << factorialNumber(8, 2) << endl;
+	//Prime(7);
+
+    int N;
+    cout << "Enter the value of N: ";
+    cin >> N;
+
+    /*cout << "Prime numbers from z to " << N << "are: " << endl;
+
+
+    for (int i = 2; i <= N; i++) {
+        if (isPrime(i)) {
+            cout << i << " " << endl;
+        }
+    }
+	return 0;*/
+
+    printFibonacci(N);
+} 
