600b057b1135de690b8d013266dcf6123cc8732c
Muhammad Inam <nudmaufon@gmail.com>
2024-11-14 23:55:03 +0500

    better-solution-finr-pair-of-sum

--- Project2/Moorse.cpp
+++ Project2/Moorse.cpp
@@ -5,7 +5,8 @@ using namespace std;
 vector<int> findPairWithTargetSum(const vector<int>& nums, int target) {
     vector<int> ans;
     int n = nums.size();
-    for (int i = 0; i < n; i++) {
+    int pairSum = 0;
+  /*  for (int i = 0; i < n; i++) {
         for (int j = i + 1; j < n; j++) {
             if (nums[i] + nums[j] == target) {
                 ans.push_back(i);
@@ -14,7 +15,27 @@ vector<int> findPairWithTargetSum(const vector<int>& nums, int target) {
             }
         }
     }
-    return ans; // Empty vector if no pair is found
+    return ans;*/ // Empty vector if no pair is found
+        
+    int i = 0;
+    int j = n - 1;
+    while (i < j) {
+        pairSum = nums[i] + nums[j];
+        if (pairSum > target) {
+            j--;
+        }
+        else if (pairSum < target) {
+            i++;
+        }
+        else {
+            ans.push_back(i);
+            ans.push_back(j);
+            return ans;
+        }
+
+    }
+    return ans;
+
 }
 
 int main() {
